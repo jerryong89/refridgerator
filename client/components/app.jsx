0a48c5f5ad4cd853fbea53eaf2b0dc30fae7cd0a
dae-hyun-kim <daehk4@uci.edu>
2020-03-18 20:27:35 +0000

    Added Header with logo - Dae Kim

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LoginHeader from './login-header';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,7 +15,9 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <LoginHeader/>
+      </div>
     );
   }
 }
