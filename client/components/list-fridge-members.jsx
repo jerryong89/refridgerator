e584f68dd2fd745df109480348c936d935832ffc
blake-ros <dev@MSI.localdomain>
2020-03-23 17:02:42 -0700

    completed get members of a fridge feature

---
+++ client/components/list-fridge-members.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+// export default class ListFridgeMembers extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       view: 'view-all-fridge-members'
+//     };
+//   }
+
+//   }
+// }
--- client/components/my-fridge-screen.jsx
+++ client/components/my-fridge-screen.jsx
@@ -7,6 +7,7 @@ export default class MyFridgeScreen extends React.Component {
 
     };
     this.changeToAddFoodScreen = this.changeToAddFoodScreen.bind(this);
+    this.changeToViewFridgeMembers = this.changeToViewFridgeMembers.bind(this);
   }
 
   changeToAddFoodScreen() {
@@ -14,10 +15,16 @@ export default class MyFridgeScreen extends React.Component {
     setViewMethod('add-food-screen');
   }
 
+  changeToViewFridgeMembers() {
+    const setViewMethod = this.props.setView;
+    setViewMethod('view-all-fridge-members');
+  }
+
   render() {
     return (
       <div className="text-center">
         <button className="btn btn-secondary mt-5" onClick={this.changeToAddFoodScreen}>Add Food</button>
+        <button className="btn btn-secondary mt-5" onClick={this.changeToViewFridgeMembers}>Fridge Members</button>
       </div>
     );
   }
