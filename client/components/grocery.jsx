c8be26118490b39115113339382a634c5b9df4c3
dae-hyun-kim <daehk4@uci.edu>
2020-03-25 23:07:35 +0000

    db import - Dae Kim

--- client/components/grocery.jsx
+++ client/components/grocery.jsx
@@ -4,8 +4,13 @@ export default function Grocery(props) {
   const product = props.foodprop;
   return product.map(foods => {
     return (
-      <div key={foods.claimId}>
-        <button className="groceryButton btn btn-secondary">{foods.foodName}{foods.userName}</button>
+      <div key={foods.claimId} value={foods.claimId} className="mt-2">
+        <div className="card d-flex justify-content-around card-color">
+          <div className="d-flex justify-content-around">
+            {foods.foodName}
+            <p>{foods.userName}</p>
+          </div>
+        </div>
       </div>
     );
   });
