bb58fab23667a4db2f9b2a5dae7c300cc133566e
blake-ros <dev@MSI.localdomain>
2020-03-26 18:09:52 -0700

    completed user can view member items front end feature

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -237,7 +237,6 @@ export default class App extends React.Component {
   }
 
   render() {
-    console.log(this.state);
     return (
       <div>
         {this.displayView()}
--- client/components/fridge-member-items.jsx
+++ client/components/fridge-member-items.jsx
@@ -4,11 +4,18 @@ export default class FridgeMemberItems extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      groceries: [],
+      view: 'fridge-member-items'
     };
   }
 
   render() {
-    return();
+    return (
+      <div className="container d-flex flex-wrap justify-content-center">
+        <h1 className="text-center">-Fridge Member-</h1>
+        <h2 className="text-center">-{this.props.userName}-</h2>
+        <div className="container">{this.props.foodName}</div>
+      </div>
+    );
   }
 }
